f309f83ef0ba096a4dd02e33ee77a2409a92e4ec
James Niyongira <lascasasjames85@gmail.com>
2023-04-24 14:48:45 -0400

    added search widget

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <header className="p-4">
+      <header className="p-4 flex justify-between">
         <a href="" className="flex items-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -21,18 +21,20 @@ function App() {
           </svg>
           <span className="font-bold text-xl">airbnc</span>
         </a>
-        <div>
+        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
           <div>Anywhere</div>
+          <div className=" border-l border-gray-300"></div>
           <div>Any week</div>
-          <div>Any week</div>
-          <button>
+          <div className=" border-l border-gray-300"></div>
+          <div>Add guest</div>
+          <button className="bg-primary text-white p-1 rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6"
+              className="w-4 h-4"
             >
               <path
                 strokeLinecap="round"
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F5385D",
+      },
+    },
   },
   plugins: [],
 };
