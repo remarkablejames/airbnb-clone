5dedbe812605dcba71e04416809187d029c4b6fe
James Niyongira <lascasasjames85@gmail.com>
2023-04-24 15:59:20 -0400

    added login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-import Header from "../components/Header";
 const Login = () => {
-  return <div>Login form goes here</div>;
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className=" -mt-8">
+        <h1 className="text-4xl text-center mb-4">Login </h1>
+        <form className="max-w-md mx-auto ">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="youremail@example.com"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          <button className="primary">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
