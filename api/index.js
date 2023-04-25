b2b3bf8700d2a6dab7bc2eca1401ecf4f71d2ead
James Niyongira <lascasasjames85@gmail.com>
2023-04-25 14:09:21 -0400

    connected to the mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.get("/test", (req, res) => {
+require("dotenv").config({ path: "./.env" });
+
+// GLOBAL MIDDLEWARES
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+// DB CONNECTION
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.post("/test", (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
   res.json({
     message: "Hello World!",
   });
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const registerUser = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/test", {
+        name,
+        email,
+        password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className=" -mt-8">
         <h1 className="text-4xl text-center mb-4">Register </h1>
-        <form className="max-w-md mx-auto ">
-          <input type="text" name="name" id="name" placeholder="John Doe" />
+        <form className="max-w-md mx-auto " onSubmit={(e) => registerUser(e)}>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <input
             type="email"
             name="email"
             id="email"
             placeholder="youremail@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             name="password"
             id="password"
             placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
